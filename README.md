2f29b2afeef628a87ee3e9aa7f2119dc32e16640
rexent-gx <rexent_gx@rexlab.tech>
2022-11-07 22:51:12 +0900

    Compatible to fetch multiple videos, using V2 API

--- README.md
+++ README.md
@@ -4,15 +4,19 @@
 ## 使い方
 Releaseページから各OS用のビルド済みファイルがダウンロードできます。`TCCrawler`（Windowsの場合は`TCCrawler.exe`）がプログラム本体です。  
 
-Collection APIを叩くにはユーザー認証が必要なので、[TwitterDeveloperPortal](https://developer.twitter.com/)で`Consumer Keys`と`Access Tokens`を発行する必要があります。
+引数`-i`にコレクションのID(`https://twitter.com/hogehoge/timelines/1234567890123456789`の`1234567890123456789`部分)を渡すことで、指定したコレクション以下のメディアをダウンロードします。  
+その他に`-o`で出力先ディレクトリ(デフォルト`./fetched/`)、`-s`で設定ファイル(デフォルト`settings.json`)を指定できます。
 
-    TCCrawler -k <Consumer Keys> -s <Consumer Secret> -t <Access Token> -a <Access Secret> -i <Collection ID> -o <Destination Path>
+使用にはTwitter APIの`API Key`・`API Secret`・`Access Token`・`Access Secret`の発行が必要です。  
+コレクションの取得に`Twitter API Standard v1.1`、メディアの取得に`Twitter API v2`を使用しています。その為、双方のキー群か、アクセスレベルが`Elevated`な`Twitter API v2`のキー群が必要です。  
+上記キー群はJSON設定ファイルに記述することで読み込まれます。設定内容は`settings.sample.json`を参考にしてください。
 
 `TCCrawler -h`でヘルプが表示されるので詳しくはそちらを参照してください。
 
+
 ## 注意事項
 - Collectionの仕様っぽいですが、先頭から約700ツイート以前のツイートは取得できません。
-- エラーハンドリングをろくにしていません。
+
 
 ## ビルド
 .NET SDKまたはDockerが使用できる環境ではビルドすることも可能です。
--- src/Program.cs
+++ src/Program.cs
@@ -1,50 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json;
 using CoreTweet;
+using Tweetinvi;
 using System.Net;
 using Kurukuru;
 using CommandLine;
-using CommandLine.Text;
 
 namespace TCCrawler
 {
     class Options
     {
-        [Option('k', "consumerkey", Required = true, HelpText = "Twitter API ConsumerKey.")]
-        public string? ConsumerKey { get; set; }
-
-
-        [Option('s', "consumersecret", Required = true, HelpText = "Twitter API ConsumerSecret.")]
-        public string? ConsumerSecret { get; set; }
-
-        [Option('t', "accesstoken", Required = true, HelpText = "Twitter API AccessToken.")]
-        public string? AccessToken { get; set; }
-
-        [Option('a', "accesssecret", Required = true, HelpText = "Twitter API AccessSecret.")]
-        public string? AccessSecret { get; set; }
+        [Option('s', "settings", Required = false, HelpText = "Settings JSON file path. Default is './settings.json'.")]
+        public string SettingsFilePath { get; set; } = "./settings.json";
 
         [Option('i', "id", Required = true, HelpText = "Twitter Collection ID. Only ID digits, no need 'custom-'.")]
         public string CollectionID { get; set; } = string.Empty;
 
-        [Option('o', "out", Required = true, HelpText = "Directory path to save images.")]
-        public string SavePath { get; set; } = string.Empty;
+        [Option('o', "out", Required = false, HelpText = "Directory path to save images. Default is './fetched/'.")]
+        public string SavePath { get; set; } = "./fetched/";
     }
 
+    class Settings
+    {
+        public APIKeys? V1 { get; set; }
+        public APIKeys V2 { get; set; } = new APIKeys();
+    }
+
+    class APIKeys
+    {
+        public string APIKey { get; set; } = string.Empty;
+        public string APIKeySecret { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
+        public string AccessTokenSecret { get; set; } = string.Empty;
+    }
+
+    class TweetMedia
+    {
+        public long TweetId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string[] Medias { get; set; } = new string[0];
+    }
+
+    class MediaPath
+    {
+        public string Url { get; set; } = string.Empty;
+        public string FilePath { get; set; } = string.Empty;
+    }
 
     class Program
     {
-        public class TargetUrl
-        {
-            public string Url { get; set; } = string.Empty;
-            public string Type { get; set; } = string.Empty;
-            public string ScreenName { get; set; } = string.Empty;
-            public long TweetId { get; set; } = 0;
-        }
-
         static async Task Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
@@ -52,37 +57,69 @@ namespace TCCrawler
             var result = (ParserResult<Options>)Parser.Default.ParseArguments<Options>(args);
             if (result.Tag == ParserResultType.Parsed)
             {
-                var parsed = (Parsed<Options>)result;
+                var parsed = result as Parsed<Options>;
+                Settings? settings = new Settings();
+
+                // Load settings json
+                try
+                {
+                    using (StreamReader reader = File.OpenText(parsed.Value.SettingsFilePath))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            settings = JsonSerializer.Deserialize<Settings>(reader.ReadToEnd());
+                            if (settings.V1 == null || settings.V1.APIKey == "") {
+                                settings.V1 = settings.V2;
+                            }
+                        }
+                    }
+                }
+                catch (System.Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw new Exception("Failed to load settings.");
+                }
+
+                // Create dest directory
+                Directory.CreateDirectory(parsed.Value.SavePath);
 
                 ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
-                Tokens tokens = Tokens.Create(parsed.Value.ConsumerKey, parsed.Value.ConsumerSecret, parsed.Value.AccessToken, parsed.Value.AccessSecret);
+                Tokens coreTokens = Tokens.Create(settings.V1.APIKey, settings.V1.APIKeySecret, settings.V1.AccessToken, settings.V1.AccessTokenSecret);
+                TwitterClient inviClient = new TwitterClient(settings.V2.APIKey, settings.V2.APIKeySecret, settings.V2.AccessToken, settings.V2.AccessTokenSecret);
 
                 DBController.ExecuteNoneQuery("CREATE TABLE IF NOT EXISTS tweets(id integer not null primary key, url text);");
 
-                (TargetUrl[] mediaUrls, string[] executeSqls) = await getAllMediaUrlsAsync(tokens, parsed.Value.CollectionID);
-                await downloadImage(mediaUrls, parsed.Value.SavePath);
+                long[] tweetIds = await getAllTweetIdsAsync(coreTokens, parsed.Value.CollectionID);
+                TweetMedia[] tweetMedias = await getAllMediaUrlsAsync(tweetIds, inviClient);
+                await downloadMediasAsync(tweetMedias, parsed.Value.SavePath);
 
-                DBController.ExecuteNoneQueryWithTransaction(executeSqls);
+                // Update DB
+                var sqlQueries = new List<string>();
+                foreach (var tweetMedia in tweetMedias)
+                {
+                    sqlQueries.Add($"INSERT INTO tweets VALUES({tweetMedia.TweetId}, 'https://twitter.com/{tweetMedia.UserName}/status/{tweetMedia.TweetId}')");
+                }
+                DBController.ExecuteNoneQueryWithTransaction(sqlQueries.ToArray());
             }
             else
             {
-                // var notParsed = (NotParsed<Options>)result;
+                // throw new Exception("Failed to parse command arguments.");
             }
         }
 
-        static async Task<(TargetUrl[], string[])> getAllMediaUrlsAsync(Tokens tokens, string collectionId)
+        static async Task<long[]> getAllTweetIdsAsync(Tokens coreTokens, string collectionId)
         {
-            List<TargetUrl> mediaUrls = new List<TargetUrl>();
-            List<string> executeSqls = new List<string>();
+            List<long> tweetIds = new List<long>();
 
-            async Task<CollectionEntriesPosition> getMediasWithCursorAsync(CollectionEntriesPosition? pos = null)
+            async Task<CollectionEntriesPosition> getIdsWithCursorAsync(CollectionEntriesPosition? pos = null)
             {
                 CollectionEntriesResult result;
 
-                if (pos == null) { result = await tokens.Collections.EntriesAsync($"custom-{collectionId}", 150, null, null, TweetMode.Extended); }
+                // Collections API
+                if (pos == null) { result = await coreTokens.Collections.EntriesAsync($"custom-{collectionId}", 150, null, null, CoreTweet.TweetMode.Extended); }
                 else
                 {
-                    result = await tokens.Collections.EntriesAsync($"custom-{collectionId}", 150, pos.MinPosition, null, TweetMode.Extended);
+                    result = await coreTokens.Collections.EntriesAsync($"custom-{collectionId}", 150, pos.MinPosition, null, CoreTweet.TweetMode.Extended);
 
                     if (result.Entries.Length == 0)
                     {
@@ -91,123 +128,191 @@ namespace TCCrawler
                     }
                 }
 
+                // Make saved tweets collection
                 List<object[]> sqlRes = DBController.ExecuteReader($"SELECT * FROM tweets");
-                List<long> recordedIds = new List<long>();
+                List<long> savedIds = new List<long>();
 
                 sqlRes.ForEach(res =>
                 {
-                    recordedIds.Add((long)res[0]);
+                    savedIds.Add((long)res[0]);
                 });
+
 
                 foreach (var entry in result.Entries)
                 {
                     // Exclude recorded tweet
-                    if (!recordedIds.Contains(entry.Tweet.Id))
+                    if (!savedIds.Contains(entry.Tweet.Id))
                     {
-                        foreach (var media in entry.Tweet.ExtendedEntities.Media)
-                        {
-                            // Video
-                            if (media.Type == "video")
-                            {
-                                // Get most high bitrate video's url
-                                string url = "";
-                                int maxBitrate = 0;
-                                foreach (var variant in media.VideoInfo.Variants)
-                                {
-                                    if (variant.Bitrate > maxBitrate)
-                                    {
-                                        url = variant.Url;
-                                        maxBitrate = variant.Bitrate ?? maxBitrate;
-                                    }
-                                }
-                                // Bitrate does not defined
-                                if (url == "") url = media.VideoInfo.Variants[0].Url;
-
-                                mediaUrls.Add(new TargetUrl() { Url = url, Type = media.Type, ScreenName = entry.Tweet.User.ScreenName, TweetId = entry.Tweet.Id });
-                            }
-                            else
-                            {
-                                // Image (or another type?)
-                                mediaUrls.Add(new TargetUrl() { Url = media.MediaUrlHttps, Type = media.Type, ScreenName = entry.Tweet.User.ScreenName, TweetId = entry.Tweet.Id });
-                            }
-                        }
-                        executeSqls.Add($"INSERT INTO tweets VALUES({entry.Tweet.Id}, 'https://twitter.com/{entry.Tweet.User.ScreenName}/status/{entry.Tweet.Id}')");
+                        tweetIds.Add(entry.Tweet.Id);
                     }
                 }
 
                 return (result.Position);
             }
 
-            await Spinner.StartAsync("Fetching image URLs...", async spinner =>
+            await Spinner.StartAsync("Fetching Tweets...", async spinner =>
             {
                 try
                 {
-                    CollectionEntriesPosition res = await getMediasWithCursorAsync();
+                    CollectionEntriesPosition res = await getIdsWithCursorAsync();
                     while (res.WasTruncated)
                     {
                         await Task.Delay(500);
-                        res = await getMediasWithCursorAsync(res);
-                        spinner.Text = "Fetching " + mediaUrls.Count + " URLs...";
+                        res = await getIdsWithCursorAsync(res);
+                        spinner.Text = "Fetching " + tweetIds.Count + " Tweets...";
                     }
 
-                    spinner.Succeed("Fetched " + mediaUrls.Count + " URLs.");
+                    spinner.Succeed("Fetched " + tweetIds.Count + " Tweets.");
                 }
-                catch (System.Exception)
+                catch (System.Exception e)
                 {
-                    spinner.Fail("Failed to get media URLs. Invalid collection's URL?");
+                    Console.WriteLine(e.Message);
+                    spinner.Fail("Failed to fetch tweet IDs.");
                     Environment.Exit(-1);
                 }
 
             });
 
-            return (mediaUrls.ToArray(), executeSqls.ToArray());
+            return (tweetIds.ToArray());
         }
 
-        static async Task downloadImage(TargetUrl url, string savePath, Spinner spinner, HttpClient httpClient, int index, DateTime dt)
+        static async Task<TweetMedia[]> getAllMediaUrlsAsync(long[] tweetIds, TwitterClient inviClient)
         {
-            // string filePath = $"{savePath}/{url.ScreenName}_{url.TimeStamp}_{index}_{Path.GetFileName(new Uri(url.Url).AbsolutePath)}";
-            // To get timestamp from ID, use:
-            // (entry.Tweet.Id >> 22) + (long)1288834974657
-            string filePath = $"{savePath}/{url.ScreenName}_{url.TweetId}_{index}{Path.GetExtension(new Uri(url.Url).AbsolutePath)}";
-            if (File.Exists(filePath))
-            {
-                spinner.Text = ($"Downloading {Path.GetFileName(filePath)}");
-                return;
-            }
+            List<TweetMedia> tweetMedias = new List<TweetMedia>();
 
-            string httpTarget;
-            if (url.Type == "video") httpTarget = url.Url;
-            else httpTarget = url.Url + ":orig";
-            using (var res = await httpClient.GetAsync(httpTarget, HttpCompletionOption.ResponseHeadersRead))
-            using (var fileStream = File.Create(filePath))
-            using (var httpStream = await res.Content.ReadAsStreamAsync())
+            await Spinner.StartAsync("Fetching media URLs...", async spinner =>
             {
-                await httpStream.CopyToAsync(fileStream);
-            }
-            spinner.Text = ($"Downloading {Path.GetFileName(filePath)}");
+                try
+                {
+                    var tweetResponses = await inviClient.TweetsV2.GetTweetsAsync(tweetIds);
 
-            TimeSpan ts = new TimeSpan(0, 0, index);
-            File.SetCreationTime(filePath, dt - ts);
-            File.SetLastWriteTime(filePath, dt - ts);
-            File.SetLastAccessTime(filePath, dt - ts);
+                    foreach (var tweet in tweetResponses.Tweets)
+                    {
+                        List<string> mediaUrls = new List<string>();
+
+                        // Collect media URLs
+                        foreach (var mediaKey in tweet.Attachments.MediaKeys)
+                        {
+                            var medias = tweetResponses.Includes.Media.Where(m => m.MediaKey == mediaKey);
+                            if (medias.Count() != 1)
+                            {
+                                throw new Exception("Linking mediakeys failed.");
+                            }
+
+                            var media = medias.First();
+                            switch (media.Type)
+                            {
+                                case "animated_gif":
+                                    {
+                                        string url = Path.ChangeExtension(
+                                            media.PreviewImageUrl.Replace("tweet_video_thumb", "tweet_video"), ".mp4"
+                                        );
+                                        mediaUrls.Add(url);
+                                        break;
+                                    }
+
+                                case "photo":
+                                    {
+                                        if (Path.GetExtension(media.Url).ToLower() == ".jpg") mediaUrls.Add($"{media.Url}:orig");
+                                        else mediaUrls.Add(media.Url);
+                                        break;
+                                    }
+
+                                default:
+                                    {
+                                        mediaUrls.Add(media.Url);
+                                        break;
+                                    }
+                            }
+                        }
+
+                        // Get author screen name
+                        var author = await inviClient.UsersV2.GetUserByIdAsync(tweet.AuthorId);
+
+                        tweetMedias.Add(new TweetMedia()
+                        {
+                            TweetId = Convert.ToInt64(tweet.Id),
+                            UserName = author.User.Username,
+                            Medias = mediaUrls.ToArray()
+                        });
+                    }
+                }
+                catch (System.Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    spinner.Fail($"Failed to fetch media URLs.");
+                    Environment.Exit(-1);
+                }
+
+                spinner.Succeed($"Fetched {tweetMedias.Count()} media URLs.");
+            });
+
+            return tweetMedias.ToArray();
         }
 
-        static async Task downloadImage(TargetUrl[] urls, string savePath)
+        static async Task downloadMediasAsync(TweetMedia[] tweetMedias, string saveDir)
         {
-            DateTime dt = DateTime.Now;
+            // Create media url and path pairs
+            var pathPairs = new List<MediaPath>();
+            foreach (var tweetMedia in tweetMedias)
+            {
+                int index = 0;
+                foreach (var media in tweetMedia.Medias)
+                {
+                    pathPairs.Add(new MediaPath()
+                    {
+                        Url = media,
+                        FilePath = $"{saveDir}/{tweetMedia.UserName}_{tweetMedia.TweetId}_{index}{Path.GetExtension(new Uri(media).AbsolutePath.Replace(":orig", ""))}" //index.ToString("00")
+                    });
+                    index++;
+                }
+            }
 
-            await Spinner.StartAsync("Start downloading...", async spinner =>
+            await Spinner.StartAsync("Preparing download...", async spinner =>
             {
                 using (var httpClient = new HttpClient(new HttpClientHandler
                 {
                     MaxConnectionsPerServer = 25
                 }))
                 {
-                    await Task.WhenAll(urls.Select((url, i) => downloadImage(url, savePath, spinner, httpClient, i, dt)));
-                    spinner.Succeed("Process finished successfully.");
+                    await Task.WhenAll(pathPairs.Select((mediaPath, i) => downloadMediaAsync(mediaPath, httpClient, spinner)));
+                    spinner.Succeed("Download is complete.");
                 }
-
             });
+
+            Spinner.Start("Update files...", spinner =>
+            {
+                // Update downloaded file's date stats to order
+                DateTime dt = DateTime.Now;
+                int index = 0;
+                foreach (var pair in pathPairs)
+                {
+                    TimeSpan ts = new TimeSpan(0, 0, index);
+                    File.SetCreationTime(pair.FilePath, dt - ts);
+                    File.SetLastWriteTime(pair.FilePath, dt - ts);
+                    File.SetLastAccessTime(pair.FilePath, dt - ts);
+                    index++;
+                }
+                spinner.Succeed("All files have been updated.");
+            });
+        }
+
+        static async Task downloadMediaAsync(MediaPath mediaPath, HttpClient httpClient, Spinner spinner)
+        {
+            // if (File.Exists(mediaPath.FilePath))
+            // {
+            //     spinner.Text = ($"Downloading {Path.GetFileName(mediaPath.FilePath)}");
+            //     return;
+            // }
+
+            // Download and save media
+            using (var res = await httpClient.GetAsync(mediaPath.Url, HttpCompletionOption.ResponseHeadersRead))
+            using (var fileStream = File.Create(mediaPath.FilePath))
+            using (var httpStream = await res.Content.ReadAsStreamAsync())
+            {
+                await httpStream.CopyToAsync(fileStream);
+            }
+            spinner.Text = ($"Downloading {Path.GetFileName(mediaPath.FilePath)}");
         }
     }
 }
